8a127ee56eee06c03a3866efd44203395ec604a8
Puspa <edith.puspa@gmail.com>
2023-04-23 18:00:10 +0545

    Added some language in the main to better understand the output

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,30 @@ using namespace std;
 int main()
 {
     Linked_List Linkedlist;
-    Linkedlist.removeFromHead();
-    Linkedlist.removeFromTail();
-    Linkedlist.traverse();
+    cout << (Linkedlist.isEmpty() ? "Linked list is empty." : "Linked list is not empty.") << endl;
+
     Linkedlist.addToTail(3);
+    cout << "Added 3 to Tail" << endl;
+
     Linkedlist.addToTail(4);
-    Linkedlist.addToTail(5);
+    cout << "Added 4 to Tail" << endl;
+
     Linkedlist.addToHead(2);
+    cout << "Added 2 to Head" << endl;
+
     Linkedlist.addToHead(1);
-    Linkedlist.addToHead(0);
+    cout << "Added 1 to Head" << endl;
+
     Linkedlist.traverse();
 
     Linkedlist.remove(3);
+    cout << "Removed 3" << endl;
     Linkedlist.traverse();
 
-    Linkedlist.removeFromTail();
+    cout << "Removed " << Linkedlist.removeFromTail() << " from Tail" << endl;
+
+    cout << "Removed " << Linkedlist.removeFromHead() << " from Head" << endl;
     Linkedlist.traverse();
 
-    Linkedlist.removeFromHead();
-    Linkedlist.traverse();
-
-    cout << Linkedlist.search(5);
+    cout << (Linkedlist.search(5) ? "5 is in the linked list." : "There is no 5 in the linked list.") << endl;
 }
